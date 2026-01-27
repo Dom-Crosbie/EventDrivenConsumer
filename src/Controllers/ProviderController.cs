fe6cd162c2aa89350e11cbbd85d16f75d0bf6e0d
Dom Crosbie <domcrosbie22@gmail.com>
2026-01-27 16:37:57 +0000

    Add /api/events endpoint to retrieve processed Kafka events

--- src/Controllers/ProviderController.cs
+++ src/Controllers/ProviderController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace Products.Controllers;
 
@@ -30,5 +32,30 @@ public class ProductsController : Controller
             return new JsonResult(product);
         }
         return new NotFoundResult();
+    }
+
+    /// <summary>
+    /// API endpoint for the demo UI to retrieve all received events
+    /// This shows what events the consumer has processed from Kafka
+    /// </summary>
+    [HttpGet]
+    [Route("/api/events")]
+    public IActionResult GetEvents()
+    {
+        // Return all products with timestamp for demo purposes
+        var products = _repository.GetProducts();
+        var events = products.Select(p => new
+        {
+            id = p.id,
+            type = p.type,
+            name = p.name,
+            version = p.version,
+            @event = "CREATED", // Simplified - in real system would track actual event type
+            timestamp = DateTime.UtcNow.AddMinutes(-products.IndexOf(p)), // Demo timestamp
+            processed = true,
+            failed = false
+        }).ToList();
+        
+        return new JsonResult(events);
     }
 }
